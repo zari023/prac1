5ba6ba0b42595bffb308d296b315ffe24bd0b4e0
zari023 <1667937@uab.cat>
2022-10-24 22:10:44 +0200

    pràctica acabada

--- p1.sh
+++ p1.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
+##########1##########
 awk -F, ' NR>1 { if ($1 ~ /^tm[0-9]*/ || /^ts[0-9]*/) print$0 }' titles.cvs > titles_net.csv
 x=$(wc -l < titles.cvs)
 y=$(wc -l < titles_net.csv)
 resta=$(($x - $y)) 
 echo $resta
 
+###########2##########
 awk -F, '$2 ~ /^[[:alnum:],",#,¡,¿,'\'']/ { print $0 }' titles_net.csv > titles_net1.csv
 
+
+##########3##########
 awk -F, '$3 ~ /MOVIE/ { print $0 }' titles_net1.csv > Movies.csv
 
 awk -F, '$3 ~ /SHOW/ { print $0 }' titles_net1.csv > Shows.csv
 
+###########4###########
 awk -F, '($12 ~ /[0-9]/) && ($13 ~ /[0-9]/) && ($14 ~ /[0-9]/) && ($15 ~ /[0-9]/) { print $0 }' Movies.csv > Movies1.csv
 
 awk -F, '($12 ~ /[0-9]/) && ($13 ~ /[0-9]/) && ($14 ~ /[0-9]/) && ($15 ~ /[0-9]/) { print $0 }' Shows.csv > Shows1.csv
 
+
+############5#########
 h=$(awk -F, 'BEGIN {max=0.0} { if ( $13*1.0 > max*1.0){ max=$13; } } END {print max}' Movies1.csv )
 
 awk -F, -v var1=$h '{ print $0","$12*($13/var1)}' Movies1.csv > Movies2.csv
@@ -30,7 +37,7 @@ awk -F, -v var3=$a '{ print $0","$12*($13/var3)}' Shows1.csv > Shows2.csv
 
 b=$(awk -F, 'BEGIN {max=0.0} { if ( $14*1.0 > max*1.0){ max=$14; } } END {print max}' Shows1.csv )
 
-awk -F, -v var4=$b '{ print $0","$12*($14/var4)}' Movies2.csv > Movies3.csv
+awk -F, -v var4=$b '{ print $0","$12*($14/var4)}' Shows2.csv > Shows3.csv
 
 head -1 titles.cvs > Movies_f.csv
 awk -F, '{print $0",""imdb_reiability"",""tmdb_reliability"}' Movies_f.csv > Movies_f1.csv
@@ -40,6 +47,7 @@ head -1 titles.cvs > Shows_f.csv
 awk -F, '{print $0",""imdb_reiability"",""tmdb_reliability"}' Shows_f.csv > Shows_f1.csv
 cat Shows3.csv >> Shows_f1.csv
 
+#############6###########
 awk -F, 'BEGIN {max=0.0} { if ( $12*1.0 > max*1.0){ max=$12; } } {if($12==max){print $1 $2 $9 $12}}' Movies_f1.csv > m12max.csv
 z=$(tail -1 m12max.csv)
 echo $z
